fd370c0e124b8df1bd1e2a0f11dafe0763d73ea0
Basharat <Basharat@DESKTOP-HRTCRUK>
2023-03-14 14:38:59 +0500

    aspnet core app added

---
+++ MyFirstAzureWebApp/Pages/About.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstAzureWebApp.Pages;
+
+public class AboutUsModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public AboutUsModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
